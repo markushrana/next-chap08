9cab00d424bd26d69c391c340811d1de917ffd41
Markush <w24016@osfl.ac.jp>
2025-05-30 12:35:10 +0900

    set revalidate site-wide

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,6 +5,8 @@ import { getNewsList } from "@/app/_libs/microcms";
 import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from '@/app/_components/ButtonLink';
+
+export const revalidate = 60 ;
 //komennto
 
 // import { News } from "@/app/_libs/microcms";
